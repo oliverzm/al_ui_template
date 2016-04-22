b6fff35ebc5f9b629335c2975a2d0a4b9e6bc65e
andres <andres@pcs4.com>
2016-04-22 22:48:57 +0100

    trailer-directive

--- src/main/tmdb/partials/movieTrailer/movieTrailerController.js
+++ src/main/tmdb/partials/movieTrailer/movieTrailerController.js
@@ -20,7 +20,7 @@ define( [ 'angular',
             var getData = function(){
                 api.movie.movie($scope.mov).then( function ( response ) {
                 $scope.details = response.data;                
-                $scope.ysrc = $sce.trustAsResourceUrl("http://www.youtube.com/embed/"+response.data.videos.results[0].key);
+                $scope.ysrc = $sce.trustAsResourceUrl("http://www.youtube.com/embed/"+response.data.videos.results[0].key+"/?rel=0&autoplay=1");
                 });
             }    
             
